bf09fa3ef0faef2faee9871096afddc8a950e3c1
Andrei.Tokmakov <andtokm@yandex.ru>
2025-01-22 16:55:45 +0400

    22_01_2025_02

--- Producers/connectors/binance_connector.py
+++ Producers/connectors/binance_connector.py
@@ -4,10 +4,11 @@ import random
 import signal
 import socket
 import string
+import sys
 import textwrap
 import time
 from enum import Enum, IntEnum
-
+import websocket
 import requests
 import multiprocessing.queues
 
@@ -15,8 +16,9 @@ from dataclasses import dataclass
 from http import HTTPStatus
 from multiprocessing import Process, Queue, Event
 from queue import Empty
-from typing import Any, List, Tuple
+from typing import Any, List, Tuple, Dict
 from requests import Response
+from websocket import WebSocket
 
 host, port = '0.0.0.0', 52525
 
@@ -27,15 +29,22 @@ class EventType(IntEnum):
 
 
 class BinanceConnector(object):
-
     MAX_PAYLOAD_SIZE: int = 1024
+    BINANCE_TESTNET_HOST: str = 'wss://testnet.binance.vision'
 
     def __init__(self):
         self.message_queue: Queue = Queue()
         self.stop_event: Event = Event()
 
         self.forwarder: Process = Process(target=self.even_forwarder)
-        self.consumer: Process = Process(target=self.event_consumer)
+        # self.consumer: Process = Process(target=self.event_consumer)
+
+        self.web_socket = websocket.WebSocketApp(f'{self.BINANCE_TESTNET_HOST}/stream',
+                                                 on_message=self.on_message,
+                                                 on_error=self.on_error,
+                                                 on_close=self.on_close,
+                                                 on_open=self.on_open,
+                                                 on_ping=self.on_ping)
 
         signal.signal(signal.SIGINT, self.signal_handler)
 
@@ -63,15 +72,44 @@ class BinanceConnector(object):
                     continue
 
     def event_consumer(self) -> None:
-        # FIXME
-        with open('/home/andtokm/DiskS/ProjectsUbuntu/FinTechPythonProjects/Producers/tests/data/depth_change.json') as file:
-            data: str = file.read()
         while True:
             if self.stop_event.is_set():
                 return
-            message: str = data
-            self.message_queue.put((EventType.DepthUpdate, message))
+            # self.message_queue.put((event_type, message))
             time.sleep(0.1)
+
+    def on_message(self,
+                   ws_socket: WebSocket,
+                   message: Any):
+        self.message_queue.put((EventType.DepthUpdate, message))
+
+    @staticmethod
+    def on_error(ws_socket: WebSocket,
+                 error: Any):
+        sys.stderr.write(f"Error: {error}")
+
+    @staticmethod
+    def on_close(ws_socket: WebSocket,
+                 close_status_code: Any,
+                 close_msg: Any):
+        print(f"WebSocket connection closed: {close_status_code} - {close_msg}")
+
+    @staticmethod
+    def on_open(ws_socket: WebSocket):
+        subscribe_message: Dict = {
+            "method": "SUBSCRIBE",
+            "params": [
+                # "btcusdt@aggTrade",
+                "btcusdt@depth"
+            ],
+            "id": 1
+        }
+        ws_socket.send(json.dumps(subscribe_message))
+
+    @staticmethod
+    def on_ping(ws_socket: WebSocket,
+                message: Any):
+        ws_socket.send(message, websocket.ABNF.OPCODE_PONG)
 
     def signal_handler(self, sig, frame):
         print('Stopping connector')
@@ -79,7 +117,8 @@ class BinanceConnector(object):
 
     def start(self):
         self.forwarder.start()
-        self.consumer.start()
+        # self.consumer.start()
+        self.web_socket.run_forever()
 
 
 # TODO: Redis instead UDP ?? https://opstree.com/blog/2019/04/16/redis-best-practices-and-performance-tuning/
@@ -87,4 +126,3 @@ class BinanceConnector(object):
 if __name__ == '__main__':
     connector: BinanceConnector = BinanceConnector()
     connector.start()
-
