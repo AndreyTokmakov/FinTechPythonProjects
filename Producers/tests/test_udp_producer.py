2d7d4a874aae7952e998863955997960a1c9bfa1
Andrei.Tokmakov <andtokm@yandex.ru>
2025-01-25 20:22:09 +0400

    25_01_2025_021

--- Producers/tests/test_udp_producer.py
+++ Producers/tests/test_udp_producer.py
@@ -2,9 +2,45 @@ import json
 import socket
 import textwrap
 import sys
+from http import HTTPStatus
 from typing import Dict, List
 
+import requests
+from requests import Response
+
+from Binance.api.api_keys import get_api_key
+from Binance.api.common import HTTPHeader
+
 host, port = '0.0.0.0', 52525
+TESTNET_API_HOST: str = 'https://testnet.binance.vision/api'
+api_key: str = get_api_key()
+
+default_headers: Dict = {'X-MBX-APIKEY': api_key}
+
+
+def send_get_request(endpoint: str,
+                     headers: Dict = None,
+                     params: Dict = None) -> Dict:
+    if not headers:
+        headers = default_headers
+    if not params:
+        params = {}
+    response: Response = requests.get(url=f'{TESTNET_API_HOST}/{endpoint}',
+                                      headers=headers,
+                                      params=params)
+    if HTTPStatus.OK == response.status_code:  # HTTP_OK
+        content_type: str = response.headers.get(HTTPHeader.ContentType)
+        if content_type and 'application/json' in content_type:
+            return response.json()
+        else:
+            return {}
+    else:
+        return {}
+
+
+def get_depth(symbol: str = 'BTCUSDT',
+              limit: int = 5000) -> Dict:
+    return send_get_request(endpoint='v3/depth', params={'symbol': symbol, 'limit': limit})
 
 
 def send_simple_json():
@@ -19,9 +55,10 @@ def send_simple_json():
         print(f'{bytes_send} bytes_send')
 
 
-def send_depth():
-    with open('/home/andtokm/DiskS/ProjectsUbuntu/FinTechPythonProjects/Producers/tests/data/depth.json') as file:
-        data: str = file.read()
+def send_depth(symbol: str = 'BTCUSDT',
+               limit: int = 10000):
+    depth: Dict = get_depth(symbol=symbol, limit=limit)
+    data: str = json.dumps({"data": depth, "s": symbol})
 
     max_size: int = 1024
     chunks: List[str] = textwrap.wrap(data, max_size)
@@ -31,7 +68,8 @@ def send_depth():
             parts -= 1
             payload: bytes = json.dumps({'p': parts, 't': 1, 'd': block}).encode('utf-8')
             bytes_send: int = sock.sendto(payload, (host, port))
-            print(f'{bytes_send} bytes_send of {len(payload)}')
+            # print(f'{bytes_send} bytes_send of {len(payload)}')
+            # print(payload)
 
 
 def send_depth_update():
@@ -47,9 +85,12 @@ def send_depth_update():
             payload: bytes = json.dumps({'p': parts, 't': 2, 'd': block}).encode('utf-8')
             bytes_send: int = sock.sendto(payload, (host, port))
             print(f'{bytes_send} bytes_send of {len(payload)}')
+            # print(payload)
 
 
 if __name__ == '__main__':
     # send_simple_json()
-    send_depth()
     # send_depth_update()
+
+    send_depth(symbol='BTCUSDT')
+    # send_depth(symbol='ETHUSDT')
