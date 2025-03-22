7193ef91ffb37459f0a915836eb79a1f11001225
andtokm <andtokm@yandex.ru>
2025-03-22 22:19:51 +0400

    22_03_2025_03

--- FlatBuffers/Sender.py
+++ FlatBuffers/Sender.py
@@ -10,3 +10,18 @@ from FlatBuffers.model.messaging import Message
 
 if __name__ == "__main__":
     print(1)
+
+    # Construct a Builder with 1024 byte backing array.
+    builder = flatbuffers.Builder(1024)
+
+    name = builder.CreateString('Some_Test_Name')
+
+    status = Message.Sta.Color().Red
+
+    # Create message
+    Message.Start(builder)
+    Message.AddName(builder, name)
+    Message.AddId(builder, 5)
+    Message.AddStatus(builder, 3)
+
+    msg = Message.End(builder)
