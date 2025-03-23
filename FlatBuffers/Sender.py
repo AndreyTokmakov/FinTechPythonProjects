fad333c0c8d685a7df1f758829def7a561d60221
Andrei.Tokmakov <andtokm@yandex.ru>
2025-03-23 07:01:17 +0400

    23_03_2025_02

--- FlatBuffers/Sender.py
+++ FlatBuffers/Sender.py
@@ -1,31 +1,39 @@
-
 import flatbuffers
 
 from FlatBuffers.model.messaging import Message
 from FlatBuffers.model.messaging.Message import Message as MessageClass
 from FlatBuffers.model.messaging import Status
 
+
 # https://flatbuffers.dev/tutorial/#python
 
-if __name__ == "__main__":
+def build_message(text: str, msg_id: int, status: Status) -> bytearray:
     # Construct a Builder with 1024 byte backing array.
     builder = flatbuffers.Builder(1024)
 
-    name = builder.CreateString('Some_Test_Name')
+    name = builder.CreateString(text)
 
     # Create message
     Message.Start(builder)
-    Message.AddName(builder, name)
-    Message.AddId(builder, 123)
-    Message.AddStatus(builder, Status.Status().Success)
+    Message.AddText(builder, name)
+    Message.AddId(builder, msg_id)
+    Message.AddStatus(builder, status)
 
     msg = Message.End(builder)
     builder.Finish(msg)
 
     data: bytearray = builder.Output()
+    return data
 
-    # print(msg)
-    #  print(data)
 
-    message: Message = MessageClass.GetRootAs(data, 0)
-    print(f'Message(name: "{message.Name().decode()}", id: {message.Id()}, status: {message.Status()})')
+def read_message(msg: Message) -> Message:
+    return MessageClass.GetRootAs(msg, 0)
+
+
+if __name__ == "__main__":
+    msg_data: bytearray = build_message(text='Some_Test_Name',
+                                        msg_id=12345,
+                                        status=Status.Status().Success)
+    message: Message = MessageClass.GetRootAs(msg_data, 0)
+
+    print(f'Message(text: "{message.Text().decode()}", id: {message.Id()}, status: {message.Status()})')
